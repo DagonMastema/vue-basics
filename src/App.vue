38dcce5129b96b9637e7b86516d8c7978e7fe128
Anindita-art <aninditajena7@gmail.com>
2019-11-06 11:24:23 +0530

    changes done to the app.vue in src

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id= "app">    
+    <HelloWorld msg="List of Employees"/>
+    <ol>
+    <li v-for="todo in todos" v-bind:key="todo">
+      {{ todo.text }}
+    </li>
+  </ol>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+//export default {
+  //name: 'app',
+  //components: {
+    //HelloWorld
+ // }  
+//}
+var app = new Vue({
+  el: '#app',
+  data: {
+    todos: [
+      { text: 'abc' },
+      { text: 'def' },
+      { text: 'xyz' }
+    ]
   }
-}
+})
 </script>
 
 <style>
