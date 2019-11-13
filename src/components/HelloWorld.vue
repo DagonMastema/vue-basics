4fbd5ce4832024d1da5cc78fdc41dbd0dc7258d1
Sai Sandeep <sandeep@geekbull.in>
2019-11-13 15:28:58 +0530

    fixed the indentation and corrected the code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,9 @@
 <template >
   <div class="hello">
-    <h1>{{ msg }}</h1>    
+    <h1>{{ msg }}</h1>
     <p>Enter value : <input v-model.lazy="message"></p>
     <p>Entered Message : {{ message }}</p>
-        
+
   <v-simple-table>
       <thead>
         <tr>
@@ -13,22 +13,22 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="item in employee" :key="item.id">
+        <tr v-for="(item, index) in employee" :key="item.id">
           <td>{{ item.id }}</td>
           <td>{{ item.name }}</td>
-          <td>{{ item.department }}</td> 
-          <td><button v-on:click="remove(item)">Remove</button></td>         
+          <td>{{ item.department }}</td>
+          <td><button v-on:click="remove(index)">Remove</button></td>
         </tr>
       </tbody>
   </v-simple-table>
   <br>
-   
+
     <input v-model.lazy="id" placeholder="id">
     <input v-model.lazy="name" placeholder="Name">
     <input v-model.lazy="department" placeholder="department">
     <br>
-    <button v-on:click="add()">Add Row</button>  
-     
+    <button v-on:click="add()">Add Row</button>
+
   </div>
 </template>
 
@@ -36,33 +36,27 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String,    
+    msg: String,
   },
   data: ()=> ({
    message: null,
    id:"",
    name:"",
    department:"",
-   
-    employee: [{
-      id: '',
-      name: '',
-      department: '',
-    }],
+   employee: [],
   }),
   methods: {
-          add() {
-              this.employee.push({
-                id: this.id,
-                name: this.name,
-                department: this.department,
-              })
-            },
-          remove(item) {  
-              var index = this.employee.indexOf(item);           
-              this.employee.splice(index,1)
-          }
-        }
+    add() {
+      this.employee.push({
+        id: this.id,
+        name: this.name,
+        department: this.department,
+      })
+    },
+    remove(index) {
+      this.employee.splice(index,1)
+    }
+  }
 }
 
 </script>
