d8161ebf8b8c3c49f2c11f37b506609f11e745b4
Anindita-art <aninditajena7@gmail.com>
2019-11-22 12:20:00 +0530

    cards

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,21 +1,32 @@
 <template>
-    <p>{{info}}</p>
+<div>
+    <!--<pre>{{info}}</pre>-->
+  <v-layout row wrap>  
+    <v-card class="ma-5" max-width="340" raised v-for="x in info" :key='x.id'>
+      <v-list-item-content class="ma-3">      
+        <v-list-item-title class="headline font-weight-bold mb-4" >{{x.title}}</v-list-item-title>        
+        <v-list-item-text>{{x.body}}</v-list-item-text>
+      </v-list-item-content>
+    </v-card> 
+  </v-layout>   
+</div>    
 </template>
 
 <script>
-import axios from 'axios' 
+import axios from 'axios'
+ 
 
 export default {
-   name: 'Test',
+    name: 'Test',    
     data () {
     return {
-      info: null
+      info: []
     }
   },
   mounted () {
     axios
-      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => (this.info = response))
+      .get('http://jsonplaceholder.typicode.com/posts')
+      .then(response => (this.info = response.data))
   }
 }
 </script>
