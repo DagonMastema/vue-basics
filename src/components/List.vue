b6fd5f89aec5c0f99c5fb11210bbac64c9eb8ddd
Anindita-art <aninditajena7@gmail.com>
2019-11-28 16:31:54 +0530

    error

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,7 +2,7 @@
 <div >
   <v-layout row>
     <v-container id="datalist">
-    <v-row v-for="(item) in employeelist" :key="item.email">
+    <v-row v-for="(item,index) in employeelist" :key="item.email">
         <v-col cols="12" md="2">          
           <v-list>{{item.firstname}}</v-list>
         </v-col>
@@ -14,7 +14,10 @@
         </v-col>
         <v-spacer></v-spacer>
         <v-col cols="12" md="2">
-          <v-btn color="amber lighten-2"  @click.stop="dialog = true">Edit</v-btn>
+          <v-btn color="amber lighten-2"  @click="edit(item,index)">Edit</v-btn>
+        </v-col>
+        <v-col cols="12" md="2">
+          <v-btn color="amber lighten-2"  @click="remove(index)">Delete</v-btn>
         </v-col>                                               
       </v-row>          
   </v-container>
@@ -26,15 +29,15 @@
       <v-card>
         <v-card-title class="headline">EDIT</v-card-title>
         <v-col >
-          <v-text-field v-model="firstname" :rules="nameRules" :counter="10" label="First name"
+          <v-text-field v-model="editeddata.firstname" :rules="nameRules" :counter="10" label="First name"
             required></v-text-field>
         </v-col>
         <v-col >
-          <v-text-field v-model="lastname" :rules="nameRules" :counter="10" label="Last name"
+          <v-text-field v-model="editeddata.lastname" :rules="nameRules" :counter="10" label="Last name"
             required></v-text-field>
         </v-col>
         <v-col >
-          <v-text-field v-model="email" :rules="emailRules" label="E-mail"></v-text-field>
+          <v-text-field v-model="editeddata.email" :rules="emailRules" label="E-mail"></v-text-field>
         </v-col>
         <v-card-actions>
           <v-spacer></v-spacer>
@@ -55,6 +58,11 @@ export default {
   data: function () {
     return {
     dialog: false,
+    editeddata: {
+      firstname: '',
+      lastname: '',
+      email: ''
+  },
     }
   },
   props: {
@@ -62,12 +70,19 @@ export default {
       type: Array,     
     }
   },
-  methods: {
-    
+  methods: {    
     edit(item){
-     item.edit = !item.edit     
-    },  
-
+     this.dialog= true     
+     this.editeddata.firstname=item.firstname
+     this.editeddata.lastname=item.lastname
+     this.editeddata.email=item.email
+     var index = this.employeelist.indexOf(item)     
+     this.employeelist.splice(index,1,this.editeddata)
+     
+    },    
+    remove(index){
+      this.employeelist.splice(index,1)
+    }  
   },
   name: 'Child',
   
